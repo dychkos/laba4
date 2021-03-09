879575ed56e7e082d32b34e374127cd21328f8fc
Sergey Dychko <76588737+dychkos@users.noreply.github.com>
2021-03-09 13:42:35 +0200

    test start

---
+++ laba4-main/src/unrgo/Main.java
@@ -0,0 +1,50 @@
+package unrgo;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    int ID = 0;
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Catalog> goods = new ArrayList<Catalog>();
+        ArrayList<Order> orders = new ArrayList<Order>();
+        Catalog start = new Catalog();
+        Order ord = new Order();
+
+        String catalogBase = "catalog.dat";
+        String orderBase = "orders.dat";
+
+        goods = start.getCatalogFromBase(catalogBase);
+
+        int chosenOperation = 0;
+        System.out.println('\t' + "Добро пожаловать в тест-версию Simple Shop");
+        System.out.println("Выберите режим:" + '\n' + "1-Админисратор магазину" + '\n' + "2-Покупатель");
+        Scanner sc = new Scanner(System.in);
+        try {
+            chosenOperation = sc.nextInt();
+        }
+        catch (Exception ex){
+            System.out.println("Ошибка ввода!");
+        }
+        switch (chosenOperation) {
+            case 1:
+                start.adminMode(goods,catalogBase,orderBase,orders);
+                break;
+            case 2:
+                ord.clientMode(goods,start, orderBase, ord);
+                break;
+
+            default:
+                System.out.println("Выбрана неверная операция!");
+                break;
+        }
+
+        System.out.println("See you!");
+
+
+    }
+}
